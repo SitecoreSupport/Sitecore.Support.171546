dd33e75047025f2b5e3b11a7e8ae23843b1d2e98
Paul Stupka <paul.stupka@sitecore.com>
2019-07-26 16:29:13 +0300

    add assembly description

--- src/Sitecore.Support.171546/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.171546/Properties/AssemblyInfo.cs
@@ -4,3 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sitecore.Support.171546")]
 [assembly: AssemblyProduct("Sitecore.Support.171546")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyDescription("When Azure Search performs a search query that contains a hyphen for items that are filtered by language, it returns the item for all languages.")]
+
